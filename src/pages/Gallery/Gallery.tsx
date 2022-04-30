0824c916046a31dfdb100095f320b2947648a542
Baiborin Petr <razor267@mail.ru>
2022-04-30 23:34:29 +0300

    Card image

--- src/pages/Gallery/Gallery.tsx
+++ src/pages/Gallery/Gallery.tsx
@@ -2,19 +2,27 @@ import React, {useEffect} from 'react'
 import {useDispatch, useSelector} from 'react-redux'
 import {actions} from '../../redux/actions'
 import {StateType} from '../../types/types'
+import {Button, Card, Col, Container, Row} from 'react-bootstrap'
 
 const Gallery = () => {
     const dispatch = useDispatch()
     const images = useSelector((state: StateType) => state.images)
 
-    useEffect(()=> {
+    useEffect(() => {
         dispatch(actions.getImages())
     }, [dispatch])
 
     return (
-        <div>
-            {images.map(item=><div key={item.id}><img src={item.url} alt={item.id.toString()}/></div>)}
-        </div>
+        <Container fluid className="d-grid mt-2">
+            <Row xs="auto" className="gap-2">
+                {images.map(item => <Col key={item.id}>
+                        <Card style={{width: '12rem'}}>
+                            <Card.Img variant="top" src={item.url}/>
+                            <Button>click</Button>
+                        </Card>
+                </Col>)}
+            </Row>
+        </Container>
     )
 }
 
