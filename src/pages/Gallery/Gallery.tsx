9d0b572f15dd7ecc226fd46434f0fecc5af7d64d
Baiborin Petr <razor267@mail.ru>
2022-05-01 22:21:35 +0300

    add catgories images

--- src/pages/Gallery/Gallery.tsx
+++ src/pages/Gallery/Gallery.tsx
@@ -1,8 +1,8 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect} from 'react'
 import {useDispatch, useSelector} from 'react-redux'
 import {actions} from '../../redux/actions'
 import {StateType} from '../../types/types'
-import {Button, Card, Col, Container, OverlayTrigger, Row} from 'react-bootstrap'
+import {Col, Container, Row} from 'react-bootstrap'
 import ImageCard from '../../components/Card'
 
 const Gallery = () => {
@@ -13,13 +13,34 @@ const Gallery = () => {
         dispatch(actions.getImages())
     }, [dispatch])
 
+    let albumNumber = 1
+    for (let i = 0; i < images.length; i++) {
+        images[i].albumId = albumNumber
+        if (albumNumber < 4) {
+            albumNumber++
+        } else {
+            albumNumber = 1
+        }
+    }
+
+    const newImages = [
+        images.filter(item => item.albumId === 1),
+        images.filter(item => item.albumId === 2),
+        images.filter(item => item.albumId === 3),
+        images.filter(item => item.albumId === 4)
+    ]
+
     return (
-        <Container fluid className="d-grid mt-2">
-            <Row xs="auto" className="gap-2">
-                {images.map(item => <Col key={item.id}>
+        <Container fluid className="d-grid mt-2 gap-3">
+            {newImages.map((album, index ) => <Row
+                className="gap-2 bg-secondary p-2"
+                key={index}
+            >
+                <Row>Альбом {index + 1}</Row>
+                <Row className="gap-2">{newImages[index].map(item => <Col key={item.id}>
                     <ImageCard url={item.url}/>
-                </Col>)}
-            </Row>
+                </Col>)}</Row>
+            </Row>)}
         </Container>
     )
 }
